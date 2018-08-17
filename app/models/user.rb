09f5e55eee35eac32d2cc1d4e81b7c74e95fc1c4
Vladimir Saponko <vis@anadeainc.com>
2018-08-17 13:21:08 +0300

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :birthdate, presence: true, age: true
+
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     firstname { 'John' }
     lastname { 'Doe' }
-    birthdate { Date.today + 16.year }
+    birthdate { Date.today - 16.year - 1.day }
     email { 'user@example.com' }
     password { 'password' }
   end
