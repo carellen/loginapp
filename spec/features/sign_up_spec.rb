6f0cf16ee7edc11616bb9d51576f6bf9b5cb1599
Vladimir Saponko <vis@anadeainc.com>
2018-08-17 13:20:36 +0300

    Add user age test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -27,4 +27,18 @@ describe "the signup process", type: :feature do
     click_button 'Sign up'
     expect(page).to have_content 'Profile'
   end
+
+  it "rejects registration for user under 16 years old" do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'Firstname', with: 'John'
+    fill_in 'Lastname', with: 'Doe'
+    select_date Date.today - 16.year, from: 'user_birthdate'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+
+    click_button 'Sign up'
+    expect(page).not_to have_content 'Profile'
+  end
 end
