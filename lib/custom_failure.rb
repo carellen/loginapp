738aac604e3c3d4279613522305712b5e9791e5e
Vladimir Saponko <vis@anadeainc.com>
2018-08-17 14:52:01 +0300

    Fix display validation messages

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,14 +1,14 @@
 class CustomFailure < Devise::FailureApp
-    def redirect_url
-       root_path
-    end
+  def redirect_url
+     root_path
+  end
 
-    # You need to override respond to eliminate recall
-    def respond
-      if http_auth?
-        http_auth
-      else
-        redirect  
-      end
+  # You need to override respond to eliminate recall
+  def respond
+    if http_auth?
+      http_auth
+    else
+      redirect
     end
   end
+end
